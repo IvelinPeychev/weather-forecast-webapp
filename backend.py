e159dd7e688ef29681235611f873a631ebf07b2f
Peychev <peyhev.vn@gmail.com>
2023-05-05 21:01:55 +0300

    - building the API by setting the key and url

--- backend.py
+++ backend.py
@@ -1,2 +1,14 @@
-def get_data(place, forecasts_day, kind):
-    pass
+import requests
+
+API_KEY = '5ca8870b01b52893554e49bf96d0d2c2'
+
+def get_data(place, forecasts_day=None, kind=None):
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+
+if __name__ == '__main__':
+    print(get_data('Tokyo'))
