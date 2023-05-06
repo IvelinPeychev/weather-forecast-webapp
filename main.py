24f43888555c0ccb5a7ef851d40890625a6240a1
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-06 11:11:33 +0300

    Day 33: - adding try / catch in case of non existing place

--- main.py
+++ main.py
@@ -21,22 +21,25 @@ st.subheader(f'{option} for the next {days} days in {place}')
 
 
 if place:
-    # Get the temperature or sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature or sky data
+        filtered_data = get_data(place, days)
 
-    if option == 'Temperature':
-        temperatures = [dict['main']['temp'] for dict in filtered_data]
-        dates = [dict['dt_txt'] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperatures (C)'})
-        st.plotly_chart(figure)
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp'] for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperatures (C)'})
+            st.plotly_chart(figure)
 
-    elif option == 'Sky':
-        sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
-        images = {'Clear': 'images/clear.png',
-                  'Clouds': 'images/cloud.png',
-                  'Rain': 'images/rain.png',
-                  'Snow': 'images/snow.png'}
-        image_paths = [images[condition] for condition in sky_condition]
-        # Create a sky plot
-        st.image(image_paths, width=115)
+        elif option == 'Sky':
+            sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {'Clear': 'images/clear.png',
+                      'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png',
+                      'Snow': 'images/snow.png'}
+            image_paths = [images[condition] for condition in sky_condition]
+            # Create a sky plot
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write('This place does not exist')
