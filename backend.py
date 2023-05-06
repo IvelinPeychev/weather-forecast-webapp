9dc006003d07e19724ade8e821a347069c303f46
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-06 10:24:40 +0300

    Day 33:  - extract and filtering the temp and sky data

--- backend.py
+++ backend.py
@@ -6,9 +6,15 @@ def get_data(place, forecasts_day=None, kind=None):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
     response = requests.get(url)
     data = response.json()
-    return data
-
+    filtered_data = data['list']
+    nr_values = 8 * forecasts_day
+    filtered_data = filtered_data[:nr_values]
+    if kind == 'Temperature':
+        filtered_data = [dict['main']['temp'] for dict in filtered_data]
+    if kind == 'Sky':
+        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+    return filtered_data
 
 
 if __name__ == '__main__':
-    print(get_data('Tokyo'))
+    print(get_data('Tokyo', 3, 'Temperature'))
